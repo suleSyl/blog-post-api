aaa76dea3777d8979641f0f8520bc81942878f70
Şule Söylemezoğlu <sule.syl11@gmail.com>
2022-02-22 23:49:04 +0300

    A test class to check basic transactions is added

--- src/test/java/com/zenigma/blogpostapi/ApplicationTests.java
+++ src/test/java/com/zenigma/blogpostapi/ApplicationTests.java
@@ -1,13 +1,65 @@
 package com.zenigma.blogpostapi;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.zenigma.blogpostapi.models.Author;
+import com.zenigma.blogpostapi.models.Post;
+import com.zenigma.blogpostapi.repository.AuthorRepository;
+import com.zenigma.blogpostapi.repository.PostRepository;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    PostRepository postRepository;
+    
+    @Autowired
+    AuthorRepository authorRepository;
+    
     @Test
-    void contextLoads() {
+    public void testCreateAuthor() {
+    	Author author = new Author.AuthorBuilder("Tuba", "Şahin", "tuba@gmail.com", "tubasahin").phoneNumber("5321234567").address("İzmir").build();
+    	authorRepository.save(author);
+    	assertNotNull(authorRepository.findById(author.getId()));
     }
-
+    
+    @Test
+    public void testCreatePost() {
+    	Author author = new Author.AuthorBuilder("Leyla", "Kara", "kubra@gmail.com", "kubrakara").phoneNumber("5321234567").address("Bursa").build();
+    	authorRepository.save(author);
+    	Post post = new Post();
+    	post.setAuthorId(author);
+    	post.setPostHeader("HeaderEx");
+    	post.setPostBody("BodyEx");
+    	postRepository.save(post);
+    	assertNotNull(postRepository.findById(post.getId()));
+    }
+    
+    @Test
+    public void testGetAllPosts() {
+    	List<Post> posts = postRepository.findAll();
+    	assertThat(posts).size().isGreaterThan(0);
+    }
+    
+    @Test
+    public void testUpdatePost() {
+    	Post post = postRepository.findById(1).get();
+    	post.setPostHeader("Header Changed");
+    	postRepository.save(post);
+    	assertNotEquals("Header", postRepository.findById(1).get().getPostHeader());
+    }
+    
+    @Test
+    public void testDeletePost() {
+    	postRepository.deleteById(1L);
+    	assertThat(postRepository.existsById(1L)).isFalse();    	
+    }
 }
